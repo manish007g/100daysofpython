0fc843ff574909162eb13e898b47e96c17833de0
‘manish007g’ <manish.max.gupta@gmail.com>
2022-12-16 19:55:46 +0530

    adding snake.py

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,28 +1,27 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 screen = Screen()
-screen.setup(width=600,height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-for position in starting_positions:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.up()
-    segment.goto(position)
-    segments.append(segment)
 
 
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(10)
+
+    snake.move()
 
 screen.exitonclick()
