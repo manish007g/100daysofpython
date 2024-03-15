e5a7bbd0f3c16ff1590296786468c75b0f2a2b30
manish007g <manish.max.gupta@gmail.com>
2024-03-15 09:43:20 +0530

    django Learn and beautiful soup

---
+++ djangoLearn/studybud/base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python !!'},
+    {'id': 2, 'name': 'Design with me !!'},
+    {'id': 3, 'name': 'Front developers !!'}
+]
+
+
+def home(request):
+    return render(request, 'home.html',{'rooms': rooms})
+
+
+def room(request):
+    return render(request, 'room.html')
